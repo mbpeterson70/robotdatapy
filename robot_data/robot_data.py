02889af8104e7b8aebd6843283560ae6396e3752
Mason Peterson <mbpeterson70@gmail.com>
2023-08-11 08:42:21 -0400

    add options for csv robot_data and interpolate btwn poses
    Signed-off-by: Mason Peterson <mbpeterson70@gmail.com>

--- robot_data/robot_data.py
+++ robot_data/robot_data.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.spatial.transform import Rotation as Rot
+from scipy.spatial.transform import Slerp
 import pandas as pd
 from rosbags.highlevel import AnyReader
 from pathlib import Path
@@ -9,7 +10,7 @@ class PoseData():
     Class for easy access to object poses over time
     """
     
-    def __init__(self, data_file, file_type, topic=None, time_tol=.1, t0=None): 
+    def __init__(self, data_file, file_type, interp=False, topic=None, time_tol=.1, t0=None, csv_options=None): 
         """
         Class for easy access to object poses over time
 
@@ -23,27 +24,45 @@ class PoseData():
                 the data_file. Defaults to None.
         """
         if file_type == 'csv':
-            self._extract_csv_data(data_file)
+            self._extract_csv_data(data_file, csv_options)
         elif file_type == 'bag':
             self._extract_bag_data(data_file, topic)
         else:
             assert False, "file_type not supported, please choose from: csv or bag2"
         self.time_tol = time_tol
+        self.interp = interp
         if t0 is not None:
             self.times -= self.times[0] + t0
     
-    def _extract_csv_data(self, csv_file):
+    def _extract_csv_data(self, csv_file, csv_options):
         """
         Extracts pose data from csv file with 9 columns for time (sec/nanosec), position, and orientation
 
         Args:
             csv_file (str): CSV file path
         """
-        pose_df = pd.read_csv(csv_file, usecols=['header.stamp.secs', 'header.stamp.nsecs', 'pose.position.x', 'pose.position.y', 'pose.position.z',
-            'pose.orientation.x', 'pose.orientation.y', 'pose.orientation.z', 'pose.orientation.w'])
-        self.positions = pd.DataFrame.to_numpy(pose_df.iloc[:, 2:5])
-        self.orientations = pd.DataFrame.to_numpy(pose_df.iloc[:, 5:9])
-        self.times = pd.DataFrame.to_numpy(pose_df.iloc[:,0:1]) + pd.DataFrame.to_numpy(pose_df.iloc[:,1:2])*1e-9
+        if csv_options is None:
+            pose_df = pd.read_csv(csv_file, usecols=['header.stamp.secs', 'header.stamp.nsecs', 'pose.position.x', 'pose.position.y', 'pose.position.z',
+                'pose.orientation.x', 'pose.orientation.y', 'pose.orientation.z', 'pose.orientation.w'])
+            self.positions = pd.DataFrame.to_numpy(pose_df.iloc[:, 2:5])
+            self.orientations = pd.DataFrame.to_numpy(pose_df.iloc[:, 5:9])
+            self.times =( pd.DataFrame.to_numpy(pose_df.iloc[:,0:1]) + pd.DataFrame.to_numpy(pose_df.iloc[:,1:2])*1e-9).reshape(-1)
+        else:
+            cols = csv_options['cols']
+            pose_df = pd.read_csv(csv_file, usecols=cols['time'] + cols['position'] + cols['orientation'])
+            
+            if 'col_nums' in csv_options:
+                t_cn = csv_options['col_nums']['time']
+                pos_cn = csv_options['col_nums']['position']
+                ori_cn = csv_options['col_nums']['orientation']
+            else:
+                t_cn, pos_cn, ori_cn = [0], [1,2,3], [4,5,6,7]
+            self.positions = pd.DataFrame.to_numpy(pose_df.iloc[:, pos_cn])
+            self.orientations = pd.DataFrame.to_numpy(pose_df.iloc[:, ori_cn])
+            self.times = pd.DataFrame.to_numpy(pose_df.iloc[:,t_cn]).astype(np.float64).reshape(-1)
+
+            if 'timescale' in csv_options:
+                self.times *= csv_options['timescale']
         return
     
     def _extract_bag_data(self, bag_file, topic):
@@ -81,7 +100,14 @@ class PoseData():
         idx = self.idx(t)
         if idx is None:
             return None
-        return self.positions[idx]
+        if self.interp:
+            if np.allclose(*self.times[idx].tolist()):
+                return self.positions[idx[0]]
+            return self.positions[idx[0]] + \
+                (self.positions[idx[1]] - self.positions[idx[0]]) * \
+                (t - self.times[idx[0]]) / (self.times[idx[1]] - self.times[idx[0]])
+        else:
+            return self.positions[idx]
     
     def orientation(self, t):
         """
@@ -96,7 +122,15 @@ class PoseData():
         idx = self.idx(t)
         if idx is None:
             return None
-        return self.orientations[idx]
+        
+        if self.interp:
+            if np.allclose(*self.times[idx].tolist()):
+                return self.orientations[idx[0]]
+            orientations = Rot.from_quat(self.orientations[idx])
+            slerp = Slerp(self.times[idx], orientations)
+            return slerp(t).as_quat()
+        else:
+            return self.orientations[idx]
     
     def T_WB(self, t):
         """
@@ -108,14 +142,13 @@ class PoseData():
         Returns:
             np.array, shape(4,4): Rigid body transform
         """
-        idx = self.idx(t)
-        if idx is None: 
+        position = self.position(t)
+        orientation = self.orientation(t)
+        if position is None or orientation is None:
             return None
-        curr_position = self.positions[idx]
-        curr_orientation = self.orientations[idx]
         T_WB = np.eye(4)
-        T_WB[:3,:3] = Rot.from_quat(curr_orientation).as_matrix()
-        T_WB[:3,3] = curr_position
+        T_WB[:3,:3] = Rot.from_quat(orientation).as_matrix()
+        T_WB[:3,3] = position
         return T_WB
     
     def idx(self, t):
@@ -129,26 +162,31 @@ class PoseData():
             int: Index of pose info closest to desired time or None if no time is available within 
                 tolerance.
         """
-        op1_exists = np.where(self.times >= t)[0].shape[0]
-        op2_exists = np.where(self.times <= t)[0].shape[0]
+        op1_exists = np.where(self.times <= t)[0].shape[0]
+        op2_exists = np.where(self.times >= t)[0].shape[0]
         if not op1_exists and not op2_exists:
             idx = None
         if op1_exists:
-            op1 = np.where(self.times >= t)[0][0]
+            op1 = np.where(self.times <= t)[0][-1]
         if op2_exists:
-            op2 = np.where(self.times <= t)[0][-1]
+            op2 = np.where(self.times >= t)[0][0]
             
         if not op1_exists: 
-            idx = op2
+            idx = op2 if not self.interp else [op2, op2]
         elif not op2_exists: 
-            idx = op1
+            idx = op1 if not self.interp else [op1, op1]
+        elif self.interp:
+            idx = [op1, op2]
         elif abs(t - self.times[op1]) < abs(t - self.times[op2]): 
             idx = op1
         else: 
             idx = op2
-            
+        
+        if self.interp:
+            return idx
+        
         # check to make sure found time is close enough
-        if (self.times[idx] - t) < self.time_tol: 
+        if abs(self.times[idx] - t) > self.time_tol: 
             return None
         else: 
             return idx
