4bd075f5e8ad129b2de994fe09eab83327f14fc7
Mason Peterson <mbpeterson70@gmail.com>
2024-03-29 09:43:39 -0400

    add aruns method implementation
    Signed-off-by: Mason Peterson <mbpeterson70@gmail.com>

--- robot_utils/transform.py
+++ robot_utils/transform.py
@@ -80,3 +80,33 @@ T_RDFFLU = np.array([
     [1, 0, 0, 0],
     [0, 0, 0, 1]
 ], dtype=np.float64)
+
+def aruns(pts1, pts2, weights=None):
+    """Aruns method for 3D registration
+
+    Args:
+        pts1 (numpy.array, shape(n,2 or 3)): initial set of points
+        pts2 (numpy.array, shape(n,2 or 3)): second set of points that should be aligned to first set
+        weights (numpy.array, shape(n), optional): weights applied to associations. Defaults to None.
+
+    Returns:
+        numpy.array, shape(3,3 or 4,4): rigid body transformation to align pts2 with pts1
+    """
+    if weights is None:
+        weights = np.ones((pts1.shape[0],1))
+    weights = weights.reshape((-1,1))
+    mean1 = (np.sum(pts1 * weights, axis=0) / np.sum(weights)).reshape(-1)
+    mean2 = (np.sum(pts2 * weights, axis=0) / np.sum(weights)).reshape(-1)
+    pts1_mean_reduced = pts1 - mean1
+    pts2_mean_reduced = pts2 - mean2
+    assert pts1_mean_reduced.shape == pts2_mean_reduced.shape
+    H = pts1_mean_reduced.T @ (pts2_mean_reduced * weights)
+    U, s, Vh = np.linalg.svd(H)
+    R = U @ Vh
+    if np.allclose(np.linalg.det(R), -1.0):
+        Vh_prime = Vh.copy()
+        Vh_prime[-1,:] *= -1.0
+        R = U @ Vh_prime
+    t = mean1.reshape((-1,1)) - R @ mean2.reshape((-1,1))
+    T = np.concatenate([np.concatenate([R, t], axis=1), np.hstack([np.zeros((1, R.shape[0])), [[1]]])], axis=0)
+    return T
