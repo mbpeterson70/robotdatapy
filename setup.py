72a5df1e53ad0b5c93a945e08d641620ce783465
Mason Peterson <mbpeterson70@gmail.com>
2024-07-10 20:17:54 -0400

    update version
    Signed-off-by: Mason Peterson <mbpeterson70@gmail.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='robotdatapy',
-    version='0.1.0',    
+    version='1.0.0',    
     description='Python package for interfacing with robot data',
     url='',
     author='Mason Peterson',
