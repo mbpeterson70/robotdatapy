c74def2019090cce12a93bddf5796d7da8517b55
Mason Peterson <64281317+mbpeterson70@users.noreply.github.com>
2026-01-09 15:03:39 -0500

    Update video output to use RGB channels only
    Modify video writer to only include RGB channels.

--- robotdatapy/data/img_data.py
+++ robotdatapy/data/img_data.py
@@ -203,7 +203,7 @@ class ImgData(RobotData):
         fourcc = cv2.VideoWriter_fourcc(*'mp4v')
         out = cv2.VideoWriter(path, fourcc, fps, (sample_img.shape[1], sample_img.shape[0]))
         for t in self.times:
-            out.write(self.img(t))
+            out.write(self.img(t)[:,:,:3])
         out.release()
         return
 
